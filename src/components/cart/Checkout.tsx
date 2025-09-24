66e3f34b86ff7b2f355662360bce20b8b3b3da7e
Your Name <you@example.com>
2025-09-24 17:01:03 +0330

    test test test

--- src/components/cart/Checkout.tsx
+++ src/components/cart/Checkout.tsx
@@ -1,9 +1,12 @@
 function Checkout() {
   return (
-    <h1 className="text-red-500 mt-90 text-2xl font-semibold">
+    <div>
+      <h1 className="text-green-500 mt-90 text-2xl font-semibold">
       DID YOU REALLY THINK THERE IS GONNA BE A REAL CHECKOUT? <br></br> YOU
       FOOL, ALL OF THIS IS FAKE. DO NOT CLICK ON ANY BUTTON OR LINK THAT YOU SEE
     </h1>
+   {/* <h1 className="mt-20 ">kdjfjfjjkfdf</h1> */}
+    </div>
   )
 }
 
